d8eb269a1b17e4e1baeaf98f44c9c6022ea12a5f
patrick1671 <107428198+patrick1671@users.noreply.github.com>
2022-09-29 17:25:52 +0200

    Update navBar.php

--- Include/navBar.php
+++ Include/navBar.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $_SESSION["lang"]="pl";
+$_SESSION["langHelper"]="pl";
 $language="Angielski";
 $URL=parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
@@ -9,9 +10,11 @@ $langTab=["Strona główna","Zaloguj się","Kontakt"];
 if(isset($_GET["lang"]) && $_GET["lang"]=="pl" ){
   
   $_SESSION["lang"]="en";
+  $_SESSION["langHelper"]="pl";
   $language="Angielski";
 }else if(isset($_GET["lang"]) && $_GET["lang"]=="en" ){
   $_SESSION["lang"]="pl";
+  $_SESSION["langHelper"]="en";
   $language="Polish";
   $langTab=["Home","Signin","Contact"];
 }
@@ -25,13 +28,14 @@ if(isset($_GET["lang"]) && $_GET["lang"]=="pl" ){
           <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
           <ul class="nav-list">
             <li>
-              <a href="../UserPanel/index.php?lang=pl"><?php echo $langTab[0];?></a>
+              
+              <a href="../UserPanel/index.php?lang=<?php echo $_SESSION["langHelper"]?>"><?php echo $langTab[0];?></a>
             </li>
             <li>
-              <a href="../Signin/loginMain.php?lang=pl"><?php echo $langTab[1];?></a>
+              <a href="../Signin/loginMain.php?lang=<?php echo $_SESSION["langHelper"]?>"><?php echo $langTab[1];?></a>
             </li>
             <li>
-              <a href="../Contact/Contact.php?lang=pl"><?php echo $langTab[2];?></a>
+              <a href="../Contact/Contact.php?lang=<?php echo $_SESSION["langHelper"]?>"><?php echo $langTab[2];?></a>
             </li>
             <li>  
               <a href="<?php echo $URL."?lang=".$_SESSION["lang"]?>"><?php echo $language ?> </a>
